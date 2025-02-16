0310dbe6fc642f42af1dfd16397aca63446d2ea0
dengxiaofeng <dengxiaofeng@ztgame.com>
2025-02-16 16:55:01 +0800

    plugin load

--- src/plugin/plugin.go
+++ src/plugin/plugin.go
@@ -82,7 +82,11 @@ type HashCheckerFunc func(modulename, linktimehash, runtimehash string) bool
 // Open opens a Go plugin.
 // If a path has already been opened, then the existing *[Plugin] is returned.
 // It is safe for concurrent use by multiple goroutines.
-func Open(path string, hashchecker HashCheckerFunc) (*Plugin, error) {
+func Open(path string) (*Plugin, error) {
+	return open(path, nil)
+}
+
+func Load(path string, hashchecker HashCheckerFunc) (*Plugin, error) {
 	return open(path, hashchecker)
 }
 
